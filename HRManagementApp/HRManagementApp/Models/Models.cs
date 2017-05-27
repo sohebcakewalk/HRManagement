f95cb25443ec5040c1274257e6b3cd102cea7130
ASIF-PC\asifs <asif@cakewalk.in>
2017-05-28 02:26:44 +0530

    Task Management created , database entity updated

---
+++ HRManagementApp/HRManagementApp/Models/Models.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HRManagementApp.Models
+{
+    public class Models
+    {
+
+
+
+
+    }
+
+
+
+    public class ModelTask
+    {
+        public long TaskId { get; set; }
+        public string GitUrl { get; set; }
+        public DateTime ? createDate { get; set; }
+        public string TaskName { get; set; }
+        public long ? UserId { get; set; }
+        public DateTime ? CompletedDate { get; set; }
+        public long ? TimeTaken { get; set; }
+    }
+
+
+}
