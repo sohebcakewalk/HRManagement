84898193f9994cfe85d29ca309f70386e46f8629
Soheb Rapati <soheb@cakewalk.in>
2017-05-28 20:48:29 +0530

    Change Logout

--- HRManagementApp/HRManagementApp/Admin/Dashboard.Master.cs
+++ HRManagementApp/HRManagementApp/Admin/Dashboard.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace HRManagementApp.Admin
 {
-    public partial class Dashboard : BaseAdminPage
+    public partial class Dashboard : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
